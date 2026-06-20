73d12fc5138f402b68f1052fb70a3fa8142c8bbf
ShuRuMingZi1145 <3859282287@qq.com@example.com>
2026-06-20 19:48:07 +0800

    Auto sync source code for Vercel 🚀

--- data/projects.ts
+++ data/projects.ts
@@ -11,14 +11,11 @@ export type Project = {
 
 export const projectsData: Project[] = [
   {
-    "id": "proj_1775049332705",
-    "name": "Computational Chemistry Tool",
-    "githubUrl": "https://github.com/heiehiehi/Computational_Chemistry_Tool",
-    "description": "该工具本作者使用在Win下的WSL2平台，系统为Ubuntu22，个人使用请依据自己数据进行修改（这些工具只是整合了一些流程）",
-    "icon": "🚀",
-    "tags": [
-      "Gromacs",
-      "RMSF"
-    ]
-  },
+    "id": "proj_1781955961861",
+    "name": "Api Tool",
+    "githubUrl": "https://github.com/ShuRuMingZi1145/Api-Tool",
+    "description": "一个通过API来实现在手机上编程的工具",
+    "icon": "💻",
+    "tags": []
+  }
 ];
---
+++ wrap-worker.mjs
@@ -0,0 +1,2 @@
+import { copyFileSync } from "fs";
+copyFileSync(".open-next/worker.js", ".open-next/_worker.js");
